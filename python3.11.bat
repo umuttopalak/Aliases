124472e4df68e006da0d7cc3a5bc1504cc9b1653
umuttopalak <umuttopalak@hotmail.com>
2023-11-12 04:29:46 +0300

    python aliases added

---
+++ python3.11.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\Users\umutt\AppData\Local\Programs\Python\Python311\python.exe %1 %2 %3
---
+++ python3.8.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\Users\umutt\AppData\Local\Programs\Python\Python38\python.exe %1 %2 %3
